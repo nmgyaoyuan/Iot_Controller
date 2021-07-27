2082b5c0e98c64608d1a7cf2842fe9a0c3c87e60
nmgyaoyuan <3552822@qq.com>
2021-07-27 14:38:19 +0800

    利用$GPRMC(或$GNRMC),获取卫星时间戳，用于校准本地时间
    注：修改了MQTT  RTK 的Json报文

--- src/main/java/cn/rh/iot/driver/RTKDriver.java
+++ src/main/java/cn/rh/iot/driver/RTKDriver.java
@@ -4,7 +4,12 @@ import cn.rh.iot.driver.base.FrameType;
 import cn.rh.iot.driver.base.IDriver;
 
 import java.nio.charset.StandardCharsets;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 /**
  * @Program: IOT_Controller
@@ -32,41 +37,68 @@ public class RTKDriver implements IDriver {
             if(data.length<5){return null;}
             String s=new String(data, StandardCharsets.US_ASCII);
             String[] valueList=s.split(",");
-            if(!valueList[0].toUpperCase().equals("$GPGGA")){
+
+            if(valueList[0].toUpperCase().equals("$GPGGA")) {
+
+                double lat;
+                double lon;
+                double alt;
+                int quality;
+
+                String sV = valueList[2];
+                lat = Integer.parseInt(sV.substring(0, 2)) + Double.parseDouble(sV.substring(2)) / 60;
+                if (valueList[3].equals("S")) {
+                    lat = -lat;
+                }
+
+                sV = valueList[4];
+                lon = Integer.parseInt(sV.substring(0, 3)) + Double.parseDouble(sV.substring(3)) / 60;
+                if (valueList[5].equals("W")) {
+                    lon = -lon;
+                }
+                quality = Integer.parseInt(valueList[6]);
+                alt = Double.parseDouble(valueList[9]);
+
+                return "\"msgId\":" + 2 + "," + System.lineSeparator() +
+                        "\"payload\":{" + System.lineSeparator() +
+                        "\"subkind\":" + 1 + System.lineSeparator() +
+                        "\"lon\":" + lon + "," + System.lineSeparator() +
+                        "\"lat\":" + lat + "," + System.lineSeparator() +
+                        "\"alt\":" + alt + "," + System.lineSeparator() +
+                        "\"qos\":" + quality + System.lineSeparator() +
+                        "}";
+
+            }else if((valueList[0].toUpperCase().equals("$GPRMC") || valueList[0].toUpperCase().equals("$GNRMC"))
+                    && valueList[2].toUpperCase().equals("A")){
+
+                //拼接UTC时间字符串
+                StringBuilder sb = new StringBuilder();
+                sb.append("20").append(valueList[9].substring(4)).append("-").append(valueList[9].substring(2,4)).append("-").append(valueList[9].substring(0,2));
+                sb.append(" ").append(valueList[1].substring(0,2)).append(":").append(valueList[1].substring(2,4)).append(":").append(valueList[1].substring(4));
+
+                //将UTC时间转换为本地时间（北京时间）
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+                sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+
+                Date utcDate = null;
+                utcDate = sdf.parse(sb.toString());
+                sdf.setTimeZone(TimeZone.getDefault());
+                String ts=sdf.format(utcDate.getTime());
+
+                //输出json
+                return "\"msgId\":" + 2 + "," + System.lineSeparator() +
+                        "\"payload\":{" + System.lineSeparator() +
+                        "\"subkind\":" + 3 + System.lineSeparator() +
+                        "\"timestamp\":" + "\""+ ts +"\""+System.lineSeparator()+
+                        "}";
+            }else{
                 return null;
             }
-
-            double lat;
-            double lon;
-            double alt;
-            int quality;
-
-            String sV=valueList[2];
-            lat=Integer.parseInt(sV.substring(0,2))+Double.parseDouble(sV.substring(2))/60;
-            if(valueList[3].equals("S")){
-                lat=-lat;
-            }
-
-            sV=valueList[4];
-            lon=Integer.parseInt(sV.substring(0,3))+Double.parseDouble(sV.substring(3))/60;
-            if(valueList[5].equals("W")){
-                lon=-lon;
-            }
-            quality=Integer.parseInt(valueList[6]);
-            alt=Double.parseDouble(valueList[9]);
-
-            return  "\"msgId\":" + 2 + "," + System.lineSeparator() +
-                    "\"payload\":{" + System.lineSeparator() +
-                    "\"lon\":" + lon + "," + System.lineSeparator() +
-                    "\"lat\":" + lat + "," + System.lineSeparator() +
-                    "\"alt\":" + alt + "," + System.lineSeparator() +
-                    "\"qos\":" + quality + System.lineSeparator() +
-                    "}";
-
         }catch (Exception ex){
             return null;
         }
     }
+
 
     @Override
     public byte[] getAskMessage() {
--- src/test/java/cn/rh/iot/IotApplicationTests.java
+++ src/test/java/cn/rh/iot/IotApplicationTests.java
@@ -1,5 +1,6 @@
 package cn.rh.iot;
 
+import cn.rh.iot.driver.RTKDriver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,16 @@ class IotApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    void TestGPRMC(){
+        String sValue="$GPRMC,024813.640,A,3158.4608,N,11848.3737,E,10.05,324.27,150706,,,A*50";
+
+        RTKDriver dr=new RTKDriver();
+
+        System.out.println(dr.decode(sValue.getBytes()));
     }
 
 }
