0b421dd98f4e0b9b3b93621dc50f1e67c6a58eae
nmgyaoyuan <3552822@qq.com>
2021-08-02 20:16:16 +0800

    LifeDriver的stateNumber属性改为字符串

--- src/test/java/cn/rh/iot/IotApplicationTests.java
+++ src/test/java/cn/rh/iot/IotApplicationTests.java
@@ -21,22 +21,4 @@ class IotApplicationTests {
         System.out.println(dr.decode(sValue.getBytes()));
     }
 
-    @Test
-    void TestHexString(){
-
-        int stateNumber=0x000F;
-
-        String s=Integer.toString(stateNumber);
-
-        System.out.println(s);
-
-        stateNumber=0x2280;
-
-        s=Integer.toString(stateNumber);
-
-        System.out.println(s);
-
-
-    }
-
 }
